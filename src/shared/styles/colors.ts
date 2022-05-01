d0447232e559f2c1b6bd8f8191cf28db43044534
Rodrigo <rodrigo@gtacsolutions.com>
2022-04-30 22:57:59 -0300

    feat(styles): add inactiveIcon and icon colors

--- src/shared/styles/colors.ts
+++ src/shared/styles/colors.ts
@@ -2,5 +2,7 @@ export default {
   placeholder: '#8d8d8d',
   screenBackground: '#102027',
   navigationBar: '#102027',
-  textPrimary: '#FFFFFF'
+  textPrimary: '#FFFFFF',
+  inactiveIcon: 'rgba(255, 255, 255, 0.8)',
+  icon: '#ffff00'
 };
