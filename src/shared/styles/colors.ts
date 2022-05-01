ad4ae17c1db3cb1c7fcf1bcc1371ae14b2cf8587
Rodrigo <rodrigo@gtacsolutions.com>
2022-04-30 22:46:06 -0300

    feat(styles): add colors

---
+++ src/shared/styles/colors.ts
@@ -0,0 +1,6 @@
+export default {
+  placeholder: '#8d8d8d',
+  screenBackground: '#102027',
+  navigationBar: '#102027',
+  textPrimary: '#FFFFFF'
+};
---
+++ src/shared/styles/metrics.ts
@@ -0,0 +1,8 @@
+import { spacing } from 'shared/utils/styles';
+
+export const metrics = {
+  screenPadding: spacing(4),
+  border: 1,
+  navigationBar: spacing(13),
+  footer: spacing(20)
+};
