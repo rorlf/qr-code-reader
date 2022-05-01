efbd581f9c501d518ab7db4d4531d7453c422f7e
Rodrigo <rodrigo@gtacsolutions.com>
2022-05-01 01:37:25 -0300

    feat(components): add Body and Title

---
+++ src/shared/components/typographies/Body/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'shared/components/core/Text';
+import { CustomTextProps } from 'shared/components/core/Text/types';
+import styles from './styles';
+
+export const Body = ({ children, style, ...textProps }: CustomTextProps) => {
+  return (
+    <Text style={[styles.text, style]} {...textProps}>
+      {children}
+    </Text>
+  );
+};
---
+++ src/shared/components/typographies/Title/styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  text: {
+    fontSize: 24,
+    fontWeight: '500'
+  }
+});
