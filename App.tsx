058b06aa8da7226fbddffc12081da80305c53183
Rodrigo <rodrigo@gtacsolutions.com>
2022-05-05 00:36:12 -0300

    feat(store): add persistence in qrCode state

--- App.tsx
+++ App.tsx
@@ -6,20 +6,23 @@ import { Provider } from 'react-redux';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { RootSiblingParent } from 'react-native-root-siblings';
-import store from 'store';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from 'store';
 import colors from 'shared/styles/colors';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <StatusBar style="light" backgroundColor={colors.navigationBar} />
-        <SafeAreaView style={{ flex: 1 }}>
-          <RootSiblingParent>
-            <AppNavigator />
-          </RootSiblingParent>
-        </SafeAreaView>
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <StatusBar style="light" backgroundColor={colors.navigationBar} />
+          <SafeAreaView style={{ flex: 1 }}>
+            <RootSiblingParent>
+              <AppNavigator />
+            </RootSiblingParent>
+          </SafeAreaView>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,10 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import qrCodeReducer from './slices/qrCodeSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage
+};
+
+const persistedQrCodeReducer = persistReducer(persistConfig, qrCodeReducer);
 
 const store = configureStore({
   reducer: {
-    qrCode: qrCodeReducer
-  }
+    qrCode: persistedQrCodeReducer
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 });
+
+const persistor = persistStore(store);
+
+export { persistor };
 
 export default store;
